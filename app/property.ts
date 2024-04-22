61ed81b714a545ca77e6ea7ee194d661a0811772
kuro0310 <167751204+kuro0310@users.noreply.github.com>
2024-04-22 08:36:38 +0000

    pokemon

--- app/property.ts
+++ app/property.ts
@@ -1,4 +1,4 @@
-export const yourName = "You";
+export const yourName = "kuro";
 export const twitterLink = "https://twitter.com/"
 export const instagramLink = "https://instagram.com/"
 export const githubLink = "https://instagram.com/"
